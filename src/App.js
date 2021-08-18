e0d898ee1e46a28e9fea8b0c88b1c209ffbb15f2
bocacode <bocacode@bocacodes-MacBook-Air.local>
2021-08-18 10:40:32 -0400

    updated user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import ProtectedForm from './components/ProtectedForm'
+import 'antd/dist/antd.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  const [token, setToken] = useState()
+  const [returningUser, setReturningUser] = useState(false)
+  
+  if(!token){
+  return(
+    <>
+    {!returningUser
+    ? <Signup setToken={setToken} setReturningUser={setReturningUser}/>
+    : <Login setToken={setToken} setReturningUser={setReturningUser} />
+  }
+  </>
+  )
 }
-
-export default App;
+return(
+ <ProtectedForm  token={token}/>
+)
+}
+  export default App
